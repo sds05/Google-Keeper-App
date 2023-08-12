0c691f0dd39489b57456641d4c946c9b0c1aad0b
Sneha Dilip <smidil.eim.ae@tamu.edu>
2023-08-12 14:57:55 +0400

    Users add notes

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
+import Note from "./Note";
+import Notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {Notes.map((notes) => (
+        <Note key={notes.id} title={notes.title} content={notes.content} />
+      ))}
+
       <Footer />
     </div>
   );
